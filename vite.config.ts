edcdc9584fb8d708aa93b2b6f868b86532f8b306
Niken Ayu <ayumichan837@gmail.com>
2024-09-18 15:02:00 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: 'pwa-offline-testing',
+        short_name: '\u0017pot\u0017',
+        description: 'pwa-offline-testing',
+        theme_color: '#11085E',
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
+});
